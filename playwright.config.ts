cf2ee99733ee0aabfb64096b86bd69a340c08722
Marianne <mariannegcecilio@gmail.com>
2025-09-01 03:42:56 +0800

    Work in progress: updates from laptop

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, devices } from '@playwright/test';
-import type { GitHubActionOptions } from "@estruyf/github-actions-reporter";
 
 /**
  * Read environment variables from file.
@@ -21,21 +20,9 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-     ["html"],
-     ["list"],
-     [
-      "@estruyf/github-actions-reporter",
-       <GitHubActionOptions>{
-         title: "Go Van E2E Test Report",
-        useDetails: true,
-        showError: true,
-      },
-   ],
-  ],
-  //reporter: 'html',
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -52,15 +39,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
